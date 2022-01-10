e095ea9024bb911b6bdfb52ac0b7e145441135e6
Kamil Niewęgłowski <kamil_nieweglowski@o2.pl>
2022-01-10 17:15:14 +0100

    Removed unnecessary positions

--- FootballMatches.Core/Enums/Positions.cs
+++ FootballMatches.Core/Enums/Positions.cs
@@ -7,17 +7,6 @@ namespace FootballMatches.Core.Enums
         [Description("Goalkeeper")] Goalkeeper = 1,
         [Description("Defender")] Defender = 2,
         [Description("Midfielder")] Midfielder = 3,
-        [Description("Forward")] Forward = 4,
-
-        //[Description("Right Full-back")] RightFullBack = 5,
-        //[Description("Left Full-back")] LeftFullBack = 6,
-        //[Description("Center-back")] CenterBack = 7,
-        //[Description("Sweeper")] Sweeper = 8,
-        //[Description("Defensive Midfielder")] DefensiveMidfielder = 9,
-        //[Description("Attacking Midfielder")] AttackingMidfielder = 10,
-        //[Description("Right Midfielder")] RightMidfielder = 11,
-        //[Description("Left Midfielder")] LeftMidfielder = 12,
-        //[Description("Center Midfielder")] CenterMidfielder = 13,
-        //[Description("Striker")] Striker = 14,
+        [Description("Forward")] Forward = 4
     }
 }
